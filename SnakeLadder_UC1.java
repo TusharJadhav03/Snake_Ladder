374ece67870f39581f16b48560c163b8ea614d3d
TUSHAR JADHAV <160405308+TusharJadhav03@users.noreply.github.com>
2024-03-28 11:04:02 +0530

    UC 1

---
+++ SnakeLadder_UC1.java
@@ -0,0 +1,16 @@
+/**
+ * SnakeLadder_UC1
+ */
+public class SnakeLadder_UC1 {
+
+    public static void main(String[] args) {
+        
+        int diceRoll = 0;
+        int current_position = 0;
+        int max_position = 100;
+
+        System.out.println("Welcome to Snake and Ladder Game!");
+
+        
+    }
+}
